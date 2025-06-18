a3ebc0e7b09fd795a3344ab11cfddf2535140a15
Gabriel Aleixo <63018864+gabriel-aleixo@users.noreply.github.com>
2025-06-18 10:53:23 +0200

    added image service configuration for sharp version 0.34.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    image: {
+        service: {
+            entrypoint: 'astro/assets/services/sharp'
+        }
+    },
     vite: {
         plugins: [],
         assetsInclude: ['**/*.svg']
